3f3f154db1176eb098af393351dcb32e87631c16
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2021-02-01 21:03:36 +0100

    Ajout du jouur.
    J'ai ajouté le joueur et ses fonctions de mouvement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+    private float movementSpeed;
+
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+        MovePlayer();
+    }
+
+    void ProcessInput()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void MovePlayer()
+    {
+        rb.velocity = movementDirection * movementSpeed * playerSpeed;
+    }
+}
