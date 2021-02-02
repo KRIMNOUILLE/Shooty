633850b1233320833ef653f4f782b9b1d3481457
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2021-02-02 21:08:46 +0100

    Amélioration du pistolet.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,8 +6,11 @@ public class Weapon : MonoBehaviour
 {
 
     public GameObject projectile;
-    public Transform shotPoint;
+    public Transform shotPoint1;
+    public Transform shotPoint2;
+    public Transform shotPos;
     public float offset;
+    public SpriteRenderer gunSprite;
 
     private float timeBtwShots;
     public float startTimeBtwShots;
@@ -17,12 +20,13 @@ public class Weapon : MonoBehaviour
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        FlipGun();
 
         if (timeBtwShots <= 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Instantiate(projectile, shotPoint.position, transform.rotation);
+                Instantiate(projectile, shotPos.position, transform.rotation);
                 timeBtwShots = startTimeBtwShots;
             }
         } else
@@ -30,5 +34,20 @@ public class Weapon : MonoBehaviour
             timeBtwShots -= Time.deltaTime;
         }
 
+    }
+
+    void FlipGun()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(mousePos.x <= transform.position.x)
+        {
+            gunSprite.flipX = true;
+            shotPos.position = shotPoint2.position;
+        } else
+        {
+            gunSprite.flipX = false;
+            shotPos.position = shotPoint1.position;
+        }
     }
 }
